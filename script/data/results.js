be56b54389976bd2178d1c146a542e7eac581c5a
rachelcorawood <rachelcorawood@gmail.com>
2023-01-19 18:12:52 -0500

    corrected data for the seed

--- script/data/results.js
+++ script/data/results.js
@@ -16,64 +16,64 @@ module.exports = [
       "Add more ghee, olive oil, and  warm spiced milks to your diet, as well as self oil massages.",
   },
   {
-    question:
+    result:
       "Add more meat to your diet if you can, cream of wheat, and warm wheat breads",
   },
   {
-    question:
+    result:
       "Add more meat to your diet if you can, cottage cheese, and cashews.",
   },
   {
-    question:
+    result:
       "Enjoyed sauteed okra, cooked black lentils, and  Psyllium husk bread",
   },
   {
-    question:
+    result:
       "Add more quinoa, barley, popcorn, sauteed bitter melon, and Tulsi Tear to your diet",
   },
   {
-    question:
+    result:
       "Enjoy only warm liquids. Have a daily ginger tea. Add fresh ginger, garlic, and black pepper to your diet.",
   },
   {
-    question:
+    result:
       "Add more cooked green vegtables to your diet, like kale, celery, green beans, etc.  Include dry ginger and black pepper in your cooking. Enjoy low-sodium and lightly buttered popcorn, unsweetened puffed rice or puffed barley when you need a snack.",
   },
   {
-    question:
+    result:
       "Enjoy more cooked green vegtables like kale, celery, arugala, green beans, sprouts etc. Add more cumin and all spice to your diet and enjoy chia tea in the morning.",
   },
   {
-    question: "Add a little pumpkin and sunflower seeds to your diet.",
+    result: "Add a little pumpkin and sunflower seeds to your diet.",
   },
   {
-    question:
+    result:
       "Add more quinoa, barley, healthy popcorn, sauteed bitter melon, cooked kale, and Tulsi Tear to your diet",
   },
   {
-    question:
+    result:
       "Enjoy some dry toast, cooked milley or barley, sauteed melon, and Tulsi tea",
   },
   {
-    question: "Enjoy a little more warm milk or cream in your diet.",
+    result: "Enjoy a little more warm milk or cream in your diet.",
   },
   {
-    question:
+    result:
       "Enjoy a little room temperture or, if excessively hot, iced water. Add wheat, barley, cucumber, and coriander into your diet.",
   },
   {
-    question:
+    result:
       "Enjoy more cheese, yogurt, black lentils, cream of wheat, ripe mangos or bananas, red meat, and if its summer time, a little ice cream at lunch time!",
   },
   {
-    question:
+    result:
       "Avoid accid foods like coffee or overly spiced foods. Add more warm milks or cream to your diet.",
   },
   {
-    question:
+    result:
       "Add more meats and wheat products to your diet if you can. Cream of wheat is a great option in the mornings!",
   },
   {
-    question: "Add a little more ghee, olive oil, and yogurt to your diet.",
+    result: "Add a little more ghee, olive oil, and yogurt to your diet.",
   },
 ];
